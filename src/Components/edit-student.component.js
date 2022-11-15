bd83be0bebe40248391f6f9cdc8769051361fab9
Yadhunandan.Probakaran <Yadhunandan.Probakaran@solera.com>
2022-11-15 08:34:10 +0000

    add edit student component

---
+++ src/Components/edit-student.component.js
@@ -0,0 +1,40 @@
+  
+import React, { useState, useEffect } from "react"; 
+import axios from "axios"; 
+import StudentForm from "./StudentForm"; 
+  
+const EditStudent = (props) => { 
+  const [formValues, setFormValues] = useState({ 
+    name: "", 
+    email: "", 
+    rollno: "", 
+  }); 
+
+  const onSubmit = (studentObject) => { 
+    axios.put("http://localhost:3000/students/update-student/" + props.match.params.id, studentObject) 
+      .then((res) => { 
+        if (res.status === 200) { 
+          alert("Student successfully updated"); 
+          props.history.push("/student-list"); 
+        } else Promise.reject(); 
+      }) 
+      .catch((err) => alert("Something went wrong")); 
+  }; 
+  
+  useEffect(() => { 
+    axios.get( "http://localhost:3000/students/update-student/" + props.match.params.id ) 
+      .then((res) => { 
+        const { name, email, rollno } = res.data; 
+        setFormValues({ name, email, rollno }); 
+      }) 
+      .catch((err) => console.log(err)); 
+  }, []); 
+  
+  return ( 
+    <StudentForm initialValues={formValues} onSubmit={onSubmit} enableReinitialize > 
+      Update Student 
+    </StudentForm> 
+  ); 
+}; 
+
+export default EditStudent;
